f2178276400ad15da3e1d85939385d497ab91c76
Joni Ngo <hangjoni@gmail.com>
2023-08-27 16:50:29 +0800

    pdf reader

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def main():
+    st.set_page_config(page_title="Chat with PDF", page_icon=":books:")
+
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Ask a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process",
+                                    accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+
+
+if __name__ == '__main__':
+    main()
