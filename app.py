5a32df25135732419fc07be221c8326e19810066
Joni Ngo <hangjoni@gmail.com>
2023-08-27 20:29:06 +0800

    working version

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from dotenv import load_dotenv
+from htmlTemplates import css, bot_template, user_template
 
 
 def get_pdf_text(pdf_docs):
@@ -11,20 +19,88 @@ def get_pdf_text(pdf_docs):
     return text
 
 
+def get_text_chunk(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=100,
+        length_function=len
+    )
+
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handleUserInput(user_question):
+    if st.session_state.uploaded_pdf:
+        response = st.session_state.conversation({'question': user_question})
+        st.session_state.chat_history = response['chat_history']
+
+        for i, message in enumerate(st.session_state.chat_history):
+            if i % 2 == 0:
+                st.write(user_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+            else:
+                st.write(bot_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+    else:
+        st.write(bot_template.replace(
+            "{{MSG}}", "Please upload a pdf file"), unsafe_allow_html=True)
+
+
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Chat with PDF", page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+    if "uploaded_pdf" not in st.session_state:
+        st.session_state.uploaded_pdf = False
 
     st.header("Chat with multiple PDFs :books:")
-    st.text_input("Ask a question about your documents:")
+
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        print(user_question)
+        handleUserInput(user_question)
 
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process",
                                     accept_multiple_files=True)
+
         if st.button("Process"):
             with st.spinner("Processing"):
                 raw_text = get_pdf_text(pdf_docs)
-                st.write(raw_text)
+                chunks = get_text_chunk(raw_text)
+                vectorstore = get_vectorstore(chunks)
+
+                conversation_chain = get_conversation_chain(vectorstore)
+
+                st.session_state.conversation = conversation_chain
+                st.session_state.uploaded_pdf = True
 
 
 if __name__ == '__main__':
